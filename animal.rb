0f6f30ca466561c379c524f8b1d2a8058bb0c85a
unrar <catbuntu@catbuntu.me>
2013-02-04 20:31:59 +0100

    Style things

--- animal.rb
+++ animal.rb
@@ -13,17 +13,17 @@ class Animal
 	end
 
 	# Return the type
-	def whichType
+	def which_type
 		return @type
 	end
 
 	# Change the type
-	def type=(newType)
-		@type = newType
+	def type=(new_type)
+		@type = new_type
 	end
 
 	# Is the animal a firefox?
-	def isFirefox?
+	def firefox?
 		if @type.downcase == "firefox"
 			return true
 		else
@@ -33,23 +33,23 @@ class Animal
 end
 
 mycat = Animal.new("Roger", 2, "cat")
-puts "#{mycat.name} is #{mycat.age}. It is a #{mycat.whichType}."
+puts "#{mycat.name} is #{mycat.age}. It is a #{mycat.which_type}."
 mycat.name = "Roxanne"
 puts "Now my cat is named #{mycat.name}."
 mycat.type = "dog"
-puts "Now #{mycat.name} is a #{mycat.whichType}."
+puts "Now #{mycat.name} is a #{mycat.which_type}."
 puts "Question: Is #{mycat.name} a firefox?"
 puts "Answer: Yes."
-puts "That's #{mycat.isFirefox?}!"
+puts "That's #{mycat.firefox?}!"
 puts "What's #{mycat.name} now?"
 newthingie = gets.chomp
 mycat.type = newthingie
-puts "Now #{mycat.name} is a #{mycat.whichType}! Do you want a new name for it? (y/n)"
+puts "Now #{mycat.name} is a #{mycat.which_type}! Do you want a new name for it? (y/n)"
 yn = gets.chomp
 if yn == "y"
 	puts "New name:"
 	nn = gets.chomp
 	mycat.name = nn
-	puts "Now your #{mycat.whichType} is a #{mycat.name}!"
+	puts "Now your #{mycat.which_type} is a #{mycat.name}!"
 end
-puts "Is #{mycat.name} a firefox? #{mycat.isFirefox?}."
+puts "Is #{mycat.name} a firefox? #{mycat.firefox?}."
